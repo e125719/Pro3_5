a1dcb1900b7729e8e736dd2d6865dfae77ff6d34
e125719 <e125719@ie.u-ryukyu.ac.jp>
2014-05-18 17:36:24 +0900

    create DB

---
+++ PowerSpot/PowerSpot/MapEntity.h
@@ -0,0 +1,17 @@
+//
+//  MapEntity.h
+//  PowerSpot
+//
+//  Created by e125719 on 2014/05/18.
+//  Copyright (c) 2014年 e125719. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface MapEntity : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * coordinate;
+
+@end
---
+++ PowerSpot/PowerSpot/MapEntity.m
@@ -0,0 +1,16 @@
+//
+//  MapEntity.m
+//  PowerSpot
+//
+//  Created by e125719 on 2014/05/18.
+//  Copyright (c) 2014年 e125719. All rights reserved.
+//
+
+#import "MapEntity.h"
+
+
+@implementation MapEntity
+
+@dynamic coordinate;
+
+@end
