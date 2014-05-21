25fab56747e22ea232cbb0de106552d073ade496
Hiroki MATSUMOTO <e125705@ie.u-ryukyu.ac.jp>
2014-05-21 20:00:14 +0900

    add DiaryViewController

---
+++ PowerSpot/PowerSpot/DiaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiaryViewController.h
+//  PowerSpot
+//
+//  Created by Hiroki MATSUMOTO on 5/21/14.
+//  Copyright (c) 2014 e125719. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiaryViewController : UIViewController
+
+@end
