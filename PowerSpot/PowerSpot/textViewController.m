477ff54198954cde7fa7e9b8e71f3716fedd5760
e125719 <e125719@ie.u-ryukyu.ac.jp>
2014-05-24 15:35:10 +0900

    add textViewController

---
+++ PowerSpot/PowerSpot/textViewController.m
@@ -0,0 +1,38 @@
+//
+//  textViewController.m
+//  PowerSpot
+//
+//  Created by e125719 on 2014/05/24.
+//  Copyright (c) 2014年 e125719. All rights reserved.
+//
+
+#import "textViewController.h"
+
+@interface textViewController ()
+
+@end
+
+@implementation textViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
