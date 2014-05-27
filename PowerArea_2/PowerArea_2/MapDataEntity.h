bb8113a543d8a387e49bc2a4b106dd3270fc74b4
e125719 <e125719@ie.u-ryukyu.ac.jp>
2014-05-27 17:47:45 +0900

    edit some

---
+++ PowerArea_2/PowerArea_2/MapDataEntity.h
@@ -0,0 +1,16 @@
+//
+//  MapDataEntity.h
+//  PowerArea_2
+//
+//  Created by e125719 on 2014/05/27.
+//  Copyright (c) 2014年 e125719. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface MapDataEntity : NSManagedObject
+
+
+@end
---
+++ PowerArea_2/PowerArea_2/MapDataEntity.m
@@ -0,0 +1,15 @@
+//
+//  MapDataEntity.m
+//  PowerArea_2
+//
+//  Created by e125719 on 2014/05/27.
+//  Copyright (c) 2014年 e125719. All rights reserved.
+//
+
+#import "MapDataEntity.h"
+
+
+@implementation MapDataEntity
+
+
+@end
