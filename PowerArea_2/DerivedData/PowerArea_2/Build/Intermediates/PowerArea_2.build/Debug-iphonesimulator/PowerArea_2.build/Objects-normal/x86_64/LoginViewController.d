a17bd10be85f5571711aa82c7792749969a3d4dc
e125719 <e125719@ie.u-ryukyu.ac.jp>
2014-06-04 13:49:10 +0900

    change presentation

---
+++ PowerArea_2/DerivedData/PowerArea_2/Build/Intermediates/PowerArea_2.build/Debug-iphonesimulator/PowerArea_2.build/Objects-normal/x86_64/LoginViewController.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/nagakuratakahiro/lecture/programingIII/works/Pro3_5/PowerArea_2/PowerArea_2/LoginViewController.m \
+  /Users/nagakuratakahiro/lecture/programingIII/works/Pro3_5/PowerArea_2/PowerArea_2/LoginViewController.h \
+  /Users/nagakuratakahiro/lecture/programingIII/works/Pro3_5/PowerArea_2/PowerArea_2/AppDelegate.h \
+  /Users/nagakuratakahiro/lecture/programingIII/works/Pro3_5/PowerArea_2/PowerArea_2/IdEntity.h
---
+++ PowerArea_2/DerivedData/PowerArea_2/Build/Intermediates/PowerArea_2.build/Debug-iphonesimulator/PowerArea_2.build/Objects-normal/x86_64/MapViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/nagakuratakahiro/lecture/programingIII/works/Pro3_5/PowerArea_2/PowerArea_2/MapViewController.m \
+  /Users/nagakuratakahiro/lecture/programingIII/works/Pro3_5/PowerArea_2/PowerArea_2/MapViewController.h
